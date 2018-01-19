5036320e7ed9555797b313e024d7dc2f5db1803d
TaegyeongLee <luckperson7@naver.com>
2018-01-19 16:15:58 +0900

    basic working

--- AccountBook-BlockChain.py
+++ AccountBook-BlockChain.py
@@ -1,11 +1,23 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
 @app.route('/')
-def hello_world():
-    return 'Hello World!'
+def home_intro():
+    return render_template('intro.html', name="home_intro")
+
+@app.route('/current')
+def current():
+    return render_template('current.html', name="current")
+
+@app.route('/home')
+def home_main():
+    return render_template('home.html', name="home_main")
+
+@app.route('/stat')
+def stat():
+    return render_template('stat.html', name="stat")
 
 
 if __name__ == '__main__':
