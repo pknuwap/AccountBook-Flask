252684a6698e5d5ea7a540aad7cac4ea9b307dc9
DESKTOP-B8C8MKU\sehunKim <sehun5515@pukyong.ac.kr>
2018-01-31 16:54:27 +0900

    add year 2018 in stat.html, add same function about 2018 chart

--- AccountBook-BlockChain.py
+++ AccountBook-BlockChain.py
@@ -3,7 +3,7 @@ from flask_paginate import Pagination
 from datetime import datetime
 from flaskext.mysql import MySQL
 from werkzeug.security import generate_password_hash, check_password_hash
-import sec  # 보안 설정
+import sec # 보안 설정
 
 mysql = MySQL()
 app = Flask(__name__)
@@ -17,24 +17,21 @@ mysql.init_app(app)
 
 app.secret_key = '???'
 
-
 # 홈화면
 @app.route('/')
 def home_intro():
     return validateLogin()
-
 
 # 회비납부 화면
 @app.route('/current')
 def current():
     try:
         if session.get('user'):
-            return render_template('current.html', userName=session.get('name'))
+            return render_template('current.html',userName=session.get('name'))
         else:
             return render_template('error.html', error="회비납부 현황을 볼 권한이 없습니다. 로그인 해주세요")
     except Exception as e:
         return render_template('error.html', error=str(e))
-
 
 # 장부화면
 @app.route('/home')
@@ -47,11 +44,11 @@ def home_main():
 
             # 정렬
             sort = request.args.get('sort')
-            if sort == "muchmoney":
+            if sort =="muchmoney":
                 cursor.callproc('sp_muchUseMoney')
             elif sort == "oldday":
                 cursor.callproc('sp_oldDay')
-            elif sort == "lastday":
+            elif sort=="lastday":
                 cursor.callproc('sp_lastDay')
             else:
                 cursor.callproc('sp_GetAccountBookAll')
@@ -96,7 +93,7 @@ def home_main():
 
             pagination = Pagination(page=page,
                                     total=len(account_list), css_framework='bootstrap4',
-                                    search=search, per_page=10, alignment="center")
+                                    search=search, per_page=10,alignment="center")
 
             return render_template('home.html', show_account_list=show_account_list, pagination=pagination,
                                    userName=session.get('name'))
@@ -110,10 +107,10 @@ def home_main():
 @app.route('/addAccount', methods=['POST'])
 def addAcount():
     try:
-        if session.get('user'):
+         if session.get('user'):
 
             _use_name = request.form['use_name']
-            _use_description = str(request.form['use_d']).replace("'", "").replace("-", "")
+            _use_description = str(request.form['use_d']).replace("'","").replace("-","")
             _use_money = request.form['use_money']
             _use_date = request.form['use_date']
             _write_name = session.get('name')
@@ -131,8 +128,7 @@ def addAcount():
             conn = mysql.connect()
             cursor = conn.cursor()
 
-            cursor.callproc('sp_addAccount',
-                            (_use_name, _use_description, _use_money, _use_date, _write_date, _write_name))
+            cursor.callproc('sp_addAccount', (_use_name, _use_description, _use_money, _use_date, _write_date, _write_name))
             data = cursor.fetchall()
 
             if len(data) is 0:
@@ -147,7 +143,7 @@ def addAcount():
 
 
 # 로그인
-@app.route('/intro', methods=['POST', 'GET'])
+@app.route('/intro', methods=['POST','GET'])
 def validateLogin():
     if request.method == 'POST':
         try:
@@ -155,20 +151,20 @@ def validateLogin():
             _password = request.form['inputPassword']
 
             # security
-            if sec.check_password(_password, 1) == False or sec.check_password(_email, 0) == False:
+            if sec.check_password(_password, 1) == False or sec.check_password(_email,0) == False:
                 return render_template('intro.html', loginError='특수문자를 제외해주세요.')
             if sec.check_null(_email) == False or sec.check_null(_password) == False:
                 return render_template('intro.html', loginError='이메일과 비밀번호를 입력해주세요.')
 
             conn = mysql.connect()
             cursor = conn.cursor()
-            cursor.callproc('sp_validateLogin', (_email,))
+            cursor.callproc('sp_validateLogin',(_email,))
             data = cursor.fetchall()
 
             if len(data) > 0:
-                if (check_password_hash(str(data[0][1]), _password)):  # user password compared
-                    session['user'] = data[0][0]  # user email
-                    session['name'] = data[0][2]  # user name
+                if(check_password_hash(str(data[0][1]), _password)): # user password compared
+                    session['user'] = data[0][0] # user email
+                    session['name'] = data[0][2] # user name
                     return render_template('intro.html', userName=str(data[0][2]))
                 else:
                     return render_template('intro.html', loginError='잘못된 Email이거나 잘못된 Password 입니다')
@@ -188,37 +184,37 @@ def validateLogin():
 # 회원가입
 @app.route('/joinIn', methods=['POST', 'GET'])
 def joinIn():
-    if request.method == 'POST':
-        try:
-            _email = request.form['inputEmail']
-            _password = request.form['inputPassword']  # 특수문자 허용 x
-            _name = request.form['inputName']
-            _gender = int(request.form['inputGender'])  # man=0, woman=1
-            _grade = int(0)  # default, 관리자=1
+        if request.method == 'POST':
+            try:
+                _email = request.form['inputEmail']
+                _password = request.form['inputPassword'] # 특수문자 허용 x
+                _name = request.form['inputName']
+                _gender = int(request.form['inputGender']) # man=0, woman=1
+                _grade = int(0) # default, 관리자=1
 
-            if _email and _password and _name:
-                conn = mysql.connect()
-                cursor = conn.cursor()
-                _hashed_password = generate_password_hash(_password)
-                cursor.callproc('sp_createUser', (_email, _hashed_password, _name, _gender, _grade))
-                data = cursor.fetchall()
+                if _email and _password and _name:
+                    conn = mysql.connect()
+                    cursor = conn.cursor()
+                    _hashed_password = generate_password_hash(_password)
+                    cursor.callproc('sp_createUser', (_email, _hashed_password, _name, _gender, _grade))
+                    data = cursor.fetchall()
 
-                if len(data) is 0:
-                    conn.commit()
-                    return render_template('joinSuccess.html', join=_name + " 님의 아이디(이메일)은 '" + _email + "' 입니다.")
+                    if len(data) is 0:
+                        conn.commit()
+                        return render_template('joinSuccess.html',join=_name + " 님의 아이디(이메일)은 '" +  _email + "' 입니다.")
+                    else:
+                        return render_template('error.html',error=str(data[0]))
                 else:
-                    return render_template('error.html', error=str(data[0]))
-            else:
-                return render_template('join.html', error="모든 항목을 다 채워주세요.")
+                    return render_template('join.html',error="모든 항목을 다 채워주세요.")
 
-        except Exception as e:
-            return render_template('error.html', error=str(e))
+            except Exception as e:
+                return render_template('error.html', error=str(e))
 
-    else:
-        if session.get('user'):
-            return render_template('error.html', error="로그아웃 후 가입을 시도해주세요.")
         else:
-            return render_template('join.html', name="join")
+            if session.get('user'):
+                return render_template('error.html', error="로그아웃 후 가입을 시도해주세요.")
+            else:
+                return render_template('join.html', name="join")
 
 
 # 통계 화면
@@ -234,36 +230,45 @@ def stat():
 
             # set value for chart
             per_month_use_money = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+            per_month_income = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+            per_month_use_frequency = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
             per_month_write_frequency = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+            per_month_budget = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
             month = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
 
             if search_option is None:
-                search_option = "2017"
+                search_option = "2018"
 
-            if search_option == "2017":
+            if search_option == "2018":
                 cursor.callproc('sp_search_stat', '0')
-            elif search_option == "2016":
+            elif search_option == "2017":
                 cursor.callproc('sp_search_stat', '1')
+            elif search_option == "2016":
+                cursor.callproc('sp_search_stat', '2')
 
             accountValue = cursor.fetchall()
-            accountValuesList = []
 
             # data Extract
 
             for account in accountValue:
                 account_use_money = account[3]
                 account_use_date = account[4]
+                account_write_date = account[5]
 
                 for date in month:
-                    if int(int(account_use_date / 100) - (int(search_option) * 100)) == date:
-                        per_month_use_money[date - 1] += int(account_use_money)
-                        per_month_write_frequency[date - 1] += 1
+                    if int(int(account_use_date/100) - (int(search_option) * 100)) == date:
+                        per_month_use_money[date-1] += int(account_use_money)
+                        per_month_use_frequency[date-1] += 1
+
+                    if int(int(account_write_date/100) - (int(search_option) * 100)) == date:
+                        per_month_write_frequency[date-1] += 1
+
 
             barValue_spend = per_month_use_money
-            lineValue_frequency = per_month_write_frequency
+            lineValue_frequency_use = per_month_use_frequency
+            lineValue_frequency_write = per_month_write_frequency
 
-            return render_template('stat.html', values=barValue_spend, Frequency_Values=lineValue_frequency,
-                                   username=session.get('user'))
+            return render_template('stat.html',values=barValue_spend, Write_Frequency_Values = lineValue_frequency_write, Use_Frequency_Values= lineValue_frequency_use, username=session.get('user'))
         else:
             return render_template('error.html', error="장부통계를 볼 권한이 없습니다. 로그인 해주세요")
     except Exception as e:
@@ -273,15 +278,14 @@ def stat():
 # 로그아웃
 @app.route('/logout')
 def logout():
-    session.pop('user', None)
+    session.pop('user',None)
     return redirect('/')
 
 
 # 에러처리,
 @app.errorhandler(404)
 def page_not_found(e):
-    return render_template('error.html', error="404 페이지를 찾을 수 없습니다")
-
+    return render_template('error.html',error="404 페이지를 찾을 수 없습니다")
 
 # 프로그램 실행
 if __name__ == '__main__':
