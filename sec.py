0e5cfee7a2f66ee8a36cdabcbff372b9c3ccd033
TaegyeongLee <luckperson7@naver.com>
2018-02-03 20:38:46 +0900

    달 체크 함수 추가

--- sec.py
+++ sec.py
@@ -10,7 +10,18 @@ def check_password(s,k):
                 return False
     return True
 
+
 def check_null(s):
     if s == "":
         return False
     return True
+
+
+def check_month(date,input_month):
+    date = str(date)
+    month = date[4:6]
+    if input_month == int(month):
+        return True
+    return False
+
+
